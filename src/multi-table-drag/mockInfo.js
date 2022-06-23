5b00ffbe01d464e80be0b646fa9b8442ff4462d0
muninnlabs <alexandreartedesigner@gmail.com>
2022-06-23 11:56:43 +0200

    mockfile added

---
+++ src/multi-table-drag/mockInfo.js
@@ -0,0 +1,45 @@
+export const attachments = [
+  {
+      "fileName": "Dummy text.docx",
+      "label": "Dummy text.docx",
+      "gridId": "628d2919ea2b3a4080cc61e8",
+      "uid": "rc-upload-1653417675348-77",
+      "fileType": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+      "fileSize": "11KB",
+      "lastModifiedDate": null,
+      "lastModifiedBy": "bruno.gomes@eurocontrol.int",
+      "visible": false,
+      "detailId": "628d2919ea2b3a4080cc61ea"
+  },
+  {
+      "fileName": "dummy.pptx",
+      "label": "dummy.pptx",
+      "gridId": "628d2919ea2b3a4080cc61eb",
+      "uid": "rc-upload-1653417675348-79",
+      "fileType": "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+      "fileSize": "29KB",
+      "lastModifiedDate": null,
+      "lastModifiedBy": "bruno.gomes@eurocontrol.int",
+      "visible": false,
+      "detailId": "628d2919ea2b3a4080cc61ed"
+  },
+  {
+      "fileName": "Dummy text.pdf",
+      "label": "Dummy text.pdf",
+      "gridId": "62a9e303ea2b3a4080cc6216",
+      "uid": "rc-upload-1655300214471-5",
+      "fileType": "application/pdf",
+      "fileSize": "11KB",
+      "lastModifiedDate": null,
+      "lastModifiedBy": "bruno.gomes@eurocontrol.int",
+      "visible": false,
+      "detailId": "62a9e2eaea2b3a4080cc6215"
+  },
+  {
+      "uid": "rc-upload-1655977992152-5",
+      "fileName": "dummy.txt",
+      "uploadKey": "8f93542443e98f41fe98e97d6d2a147193b1b005",
+      "fileType": "text/plain",
+      "label": "dummy.txt"
+  }
+]
